48511ef21b676b42fd7a1df71f529c73e7ef40ff
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-13 19:35:06 +0200

    add: colors to tailwind.config.js

--- frontend/coming-soon/tailwind.config.js
+++ frontend/coming-soon/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cozy': {
+          'cream': '#FFF5E4',
+          'peach': '#FFE3E1',
+          'rose': '#FFD1D1',
+          'dusty': '#FF9494',
+          'text': '#635985',
+        }
+      },
+      fontFamily: {
+          'sans': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
