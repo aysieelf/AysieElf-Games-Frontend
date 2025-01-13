67fc74dce7b7b6d1d82b8f192c12bd0cae79d6be
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-13 21:16:13 +0200

    update: App.jsx
    - add cozy window, plants and corner

--- frontend/coming-soon/src/App.jsx
+++ frontend/coming-soon/src/App.jsx
@@ -3,13 +3,23 @@ import Header from './components/header/Header.jsx'
 import './App.css'
 import Hero from "./components/hero/Hero.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import CozyWindow from "./components/decor/CozyWindow.jsx";
+import PottedPlants from "./components/decor/PottedPlants.jsx";
+import CozyCorner from "./components/decor/CozyCorner.jsx";
 
 function App() {
   return (
-      <div className="min-h-screen flex flex-col bg-cozy-bg">
-          <Header />
-          <Hero />
-          <Footer />
+      <div className="min-h-screen relative overflow-hidden">
+          <CozyWindow/>
+          <PottedPlants/>
+          <CozyCorner/>
+
+
+          <main className="cozy-container relative z-10 py-8">
+              <Header/>
+              <Hero/>
+              <Footer/>
+          </main>
       </div>
   )
 }
