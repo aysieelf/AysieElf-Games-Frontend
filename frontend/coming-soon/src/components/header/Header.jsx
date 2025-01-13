977e01a68f5eb635ae0ceee9def9b44c2b57e1fa
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-13 19:29:11 +0200

    add: header

---
+++ frontend/coming-soon/src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="py-4 px-6 bg-gray-800 text-white">
+            <h1 className="text=2xl font-bold">AysieElf Games</h1>
+        </header>
+    );
+}
+
+export default Header;
