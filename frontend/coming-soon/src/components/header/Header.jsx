2188dc432050ef654ddbf948c7766d8a216915f4
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-13 20:39:48 +0200

    update: Header.jsx

--- frontend/coming-soon/src/components/header/Header.jsx
+++ frontend/coming-soon/src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 function Header() {
-    return (
-        <header className="py-4 px-6 bg-cozy-cream">
-            <h1 className="font-pixel text-2xl text-cozy-text text-center tracking-wider">AysieElf Games</h1>
-        </header>
-    );
+  return (
+    <header className="py-6 px-8">
+      <h1 className="font-pixel text-4xl text-gaming-pastel-pink text-center tracking-wider">
+        AysieElf Games
+      </h1>
+    </header>
+  );
 }
 
 export default Header;
