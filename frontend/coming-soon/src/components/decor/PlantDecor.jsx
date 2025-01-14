0c8b34166d1515927a84a33dde8c44ae778947b2
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-14 14:19:17 +0200

    resize: plant

--- frontend/coming-soon/src/components/decor/PlantDecor.jsx
+++ frontend/coming-soon/src/components/decor/PlantDecor.jsx
@@ -1,33 +1,33 @@
 const PlantDecor = () => {
   return (
-    <div className="fixed bottom-4 left-4">
+    <div className="fixed bottom-4 left-4 w-24">
       <img
         src="/assets/plant/base.png"
         alt="Plant base"
-        className="relative z-0"
+        className="relative z-[5] w-full"
       />
       <img
         src="/assets/plant/leaf1.png"
         alt=""
-        className="absolute top-0 left-0 z-[1] plant-sway"
+        className="absolute top-0 left-0 z-[1] plant-sway w-full"
         style={{animationDelay: '0.1s'}}
       />
       <img
         src="/assets/plant/leaf2.png"
         alt=""
-        className="absolute top-0 left-0 z-[2] plant-sway"
+        className="absolute top-0 left-0 z-[2] plant-sway w-full"
         style={{animationDelay: '0.3s'}}
       />
       <img
         src="/assets/plant/leaf3.png"
         alt=""
-        className="absolute top-0 left-0 z-[3] plant-sway"
+        className="absolute top-0 left-0 z-[3] plant-sway w-full"
         style={{animationDelay: '0.2s'}}
       />
       <img
         src="/assets/plant/leaf4.png"
         alt=""
-        className="absolute top-0 left-0 z-[4] plant-sway"
+        className="absolute top-0 left-0 z-[4] plant-sway w-full"
         style={{animationDelay: '0s'}}
       />
     </div>
