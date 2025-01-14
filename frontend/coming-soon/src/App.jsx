e6a081934b51047c21869ce5355c51aa51b5f331
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-14 11:57:24 +0200

    update: App.jsx - remove CozyWindow - it's part of the header now - remove padding from main class - import PlantDecor component

--- frontend/coming-soon/src/App.jsx
+++ frontend/coming-soon/src/App.jsx
@@ -3,19 +3,17 @@ import Header from './components/header/Header.jsx'
 import './App.css'
 import Hero from "./components/hero/Hero.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import CozyWindow from "./components/decor/CozyWindow.jsx";
-import PottedPlants from "./components/decor/PottedPlants.jsx";
 import CozyCorner from "./components/decor/CozyCorner.jsx";
+import PlantDecor from "./components/decor/PlantDecor.jsx";
 
 function App() {
   return (
       <div className="min-h-screen relative overflow-hidden">
-          <CozyWindow/>
-          <PottedPlants/>
           <CozyCorner/>
+          <PlantDecor/>
 
 
-          <main className="cozy-container relative z-10 py-8">
+          <main className="cozy-container relative z-10 py-8 px-0">
               <Header/>
               <Hero/>
               <Footer/>
