a64bc12806f5659d2128a9ad9cfb086ef09215f3
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-16 12:11:27 +0200

    feat: Implement basic response: {"status": "ok", "timestamp": "<current_time>"}

--- backend/main.py
+++ backend/main.py
@@ -5,4 +5,7 @@ app = FastAPI()
 
 @app.get("/health", status_code=200)
 async def health_check():
-    pass
+    return {
+        "status": "ok",
+        "time_stamp": datetime.now().isoformat()
+    }
