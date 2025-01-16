8b15993e1fddf1083bcd985f08be3309521f28fb
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-16 12:13:47 +0200

    feat: Add CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from datetime import datetime
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health", status_code=200)
 async def health_check():
