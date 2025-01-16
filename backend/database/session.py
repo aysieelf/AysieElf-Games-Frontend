1907d522f815d06c5a95b9e8db3e377dc438b061
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-16 14:30:45 +0200

    add: session.py - declare an engine - create a session - define a function init_db to be called in main.py

---
+++ backend/database/session.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from src.core.config import settings
+from src.models.base import Base
+
+# engine is the connection between backend and database
+engine = create_engine(
+    settings.DATABASE_URL,
+    echo=settings.DEBUG,  # only if DEBUG = True
+    connect_args={"options": "-c timezone=UTC"} if "postgresql" in settings.DATABASE_URL else {}
+)
+
+# create a session - current transaction
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def init_db():
+    """
+    Creates tables from all models
+    """
+    Base.metadata.create_all(bind=engine)
