69dd7d41f3e32d8ddde8569a27c7d41f632be1c2
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-16 16:47:15 +0200

    refactor: frontend

--- backend/database/create_database.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE aysieelf_games
-    WITH
-    OWNER = ayselkaradayi
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
--- backend/src/models/base.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-# we declare a Base, that is later used to init the tables
-Base = declarative_base()
