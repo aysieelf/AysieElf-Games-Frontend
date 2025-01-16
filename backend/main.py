fecbfe0b016d17d3b1e5de9ccbf7ac6e3f733f30
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-16 14:47:27 +0200

    update: main.py - define App class with settings and api_router, initting db - create app function added

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,59 @@
+from contextlib import asynccontextmanager
 from datetime import datetime
 
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, APIRouter
 from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
-
-# Cross-Origin Resource Sharing
-# Example: http://localhost:3000 <=> http://localhost:8000
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+from database.session import init_db
+from src.core.config import Settings, settings
+from src.api.v1.routes import api_router
 
 
-@app.get("/health", status_code=200)
-async def health_check():
-    return {"status": "ok", "time_stamp": datetime.now().isoformat()}
+
+
+class App:
+    def __init__(
+        self, settings: Settings = settings, api_router: APIRouter = api_router
+    ):
+        self.__app = FastAPI(
+            title=settings.PROJECT_NAME,
+            version="0.1.0",
+            docs_url="/docs",
+            redoc_url="/redoc",
+            openapi_url="/openapi.json",
+            lifespan=self.lifespan,
+        )
+        self.__setup_middlewares(settings=settings)
+        self.__setup_routes(settings=settings, router=api_router)
+
+    def __setup_middlewares(self, settings: Settings):
+        # TODO: To be concretely defined in production
+        self.__app.add_middleware(
+            CORSMiddleware,
+            allow_origins=settings.CORS_ALLOWED_HOSTS,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
+    def __setup_routes(self, router: APIRouter, settings: Settings):
+        self.__app.include_router(router, prefix=settings.API_V1_STR)
+
+    @asynccontextmanager
+    async def lifespan(self, app: FastAPI):
+        init_db()
+        yield
+
+    def __call__(self):
+        return self.__app
+
+
+def create_app() -> FastAPI:
+    return App(settings=settings, api_router=api_router)()
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
