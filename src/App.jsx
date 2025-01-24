91b626cf5b343b89d9a5484a43a1773a927546a8
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-24 19:14:00 +0200

    update: App.jsx - add routers - add /login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import { useState } from 'react'
 import Header from './components/header/Header.jsx'
 import './App.css'
 import Hero from "./components/hero/Hero.jsx";
 import CozyCorner from "./components/decor/CozySocials.jsx";
 import PlantDecor from "./components/decor/PlantDecor.jsx";
+import Login from "./components/hero/Login.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-      <div className="min-h-screen relative overflow-hidden">
-          <CozyCorner/>
-          <PlantDecor/>
+      <BrowserRouter>
+          <Routes>
+              <Route path={"/"} element={
+                  <div className="min-h-screen relative overflow-hidden">
+                      <CozyCorner/>
+                      <PlantDecor/>
 
 
-          <main className="cozy-container relative z-10 py-8 px-0">
-              <Header/>
-              <Hero/>
-          </main>
-      </div>
+                      <main className="cozy-container relative z-10 py-8 px-0">
+                          <Header/>
+                          <Hero/>
+                      </main>
+                  </div>
+              }/>
+                <Route path={"/login"} element={
+                    <div className="min-h-screen relative overflow-hidden">
+                        <CozyCorner/>
+                        <PlantDecor/>
+
+
+                        <main className="cozy-container relative z-10 py-8 px-0">
+                            <Header/>
+                            <Login/>
+                        </main>
+                    </div>
+                }/>
+          </Routes>
+      </BrowserRouter>
   )
 }
 
