76f9bcd28aa505c54d5965e4050dddacbef4da43
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-24 21:57:20 +0200

    add: Floor component on the website

---
+++ src/components/decor/Floor.jsx
@@ -0,0 +1,9 @@
+const Floor = () => {
+    return (
+        <div className="fixed bottom-0 h-10 w-dvw border-4 border-cozy-text-mid-brown bg-cozy-text-mid-brown">
+
+        </div>
+    );
+}
+
+export default Floor;
